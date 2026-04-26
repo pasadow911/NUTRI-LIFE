321df114245ac7637324c3e8ebe783d308006d3b
pasadow911 <asadov911@gmail.com>
2026-04-26 04:53:01 -0400

    Refactor main.tsx to include authentication providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
-import App from "./App";
-import "./App.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import './index.css'
+import { TRPCProvider } from "@/providers/trpc"
+import { GuestAuthProvider } from "@/hooks/useGuestAuth"
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <GuestAuthProvider>
+        <TRPCProvider>
+          <App />
+        </TRPCProvider>
+      </GuestAuthProvider>
     </BrowserRouter>
-  </StrictMode>
-);
+  </StrictMode>,
+)
